121b081635e8e39fa8a314081dfe29e1beb4ff93
Sheeyx <sheeyx00@gmail.com>
2024-12-14 00:46:02 +0900

    feat: develop member schema model

---
+++ apps/edusmart-api/src/libs/enums/member.enum.ts
@@ -0,0 +1,17 @@
+export enum MemberType {
+	STUDENT = 'STUDENT',
+	TEACHER = 'TEACHER',
+	ADMIN = 'ADMIN',
+}
+
+export enum MemberStatus {
+	ACTIVE = 'ACTIVE',
+	DELETE = 'DELETE',
+	BLOCK = 'BLOCK',
+}
+
+export enum MemberAuthType {
+	PHONE = 'PHONE',
+	EMAIL = 'EMAIL',
+	TELEGRAM = 'TELEGRAM',
+}
