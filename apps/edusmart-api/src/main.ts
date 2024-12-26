a0ddffce9393fefe8aa469a851d0a55b2cbc80d0
michael <abdulazizklichev1@gmail.com>
2024-12-26 21:43:01 +0900

    feat:integrate cors

--- apps/edusmart-api/src/main.ts
+++ apps/edusmart-api/src/main.ts
@@ -8,6 +8,7 @@ import * as express from 'express';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+	app.enableCors({ origin: true, credentials: true });
   app.useGlobalInterceptors(new LoggingInterceptor());
   app.useGlobalFilters(new GlobalExceptionFilter());
   app.use('/uploads', express.static('./uploads'));
