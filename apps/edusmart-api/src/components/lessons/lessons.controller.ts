bc0d54d0c864f3c302e468a1234276883b2d1ba1
Sheeyx <sheeyx00@gmail.com>
2025-01-18 08:36:04 +0900

    fix: add video downloader to lessson component

--- apps/edusmart-api/src/components/lessons/lessons.controller.ts
+++ apps/edusmart-api/src/components/lessons/lessons.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post, Query, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query, UploadedFile, UseGuards, UseInterceptors } from '@nestjs/common';
 import { AuthGuard } from '../auth/guards/auth.guard';
 import { AuthMember } from '../auth/decorators/auth.member.decorator';
 import { ObjectId } from 'mongoose';
@@ -9,6 +9,9 @@ import { WithoutGuard } from '../auth/guards/without.guard';
 import { BoardArticle } from '../../libs/dto/board-article/board-article';
 import { shapeIntoMongoObjectId } from '../../libs/config';
 import { LessonsUpdate } from '../../libs/dto/lessons/lessons-update';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { getVideoUploader } from '../../libs/utils/video.uploader';
+
 
 @Controller('lessons')
 export class LessonsController {
@@ -17,13 +20,21 @@ export class LessonsController {
     
     @UseGuards(AuthGuard)
 	@Post('create')
-	public async createBoardArticle(
-		@Body('input') input: LessonsInput,
+	@UseInterceptors(FileInterceptor('lessonVideo', getVideoUploader('lesson')))
+	public async createLesson(
+		@Body() input: LessonsInput,
 		@AuthMember('_id') memberId: ObjectId,
+		@UploadedFile() file: Express.Multer.File,
 	): Promise<Lessons> {
+		const uploadPath = `./uploads/lesson/`; // Path ni dinamik tarzda kiritish
+		if (file) {
+			input.lessonVideo = `${uploadPath}/${file.filename}`;
+		  }
 		console.log('POST: createlessons', input);
 		return this.lessonService.createLessons(memberId, input);
 	}
+
+	
 
     @UseGuards(WithoutGuard)
     @Get('get')
@@ -38,18 +49,18 @@ export class LessonsController {
 
 	@UseGuards(AuthGuard)
     @Post("update")
-	public async updateBoardArticle(
+	public async updateLesson(
 		@Body('input') input: LessonsUpdate,
 		@AuthMember('_id') memberId: ObjectId,
 	): Promise<Lessons> {
         console.log('memberId', memberId)
-		console.log('POST: updateBoardArticle');
+		console.log('POST: updateLesson');
 		input._id = shapeIntoMongoObjectId(input._id);
 		return await this.lessonService.updateLesson(memberId, input);
 	}
 
 	@UseGuards(AuthGuard)
-	@Post('likeTargetBoardArticle')
+	@Post('likeTargetLesson')
 	public async likeTargetBoardArticle(
 		@Body('lessonId') input: string,
 		@AuthMember('_id') memberId: ObjectId,
