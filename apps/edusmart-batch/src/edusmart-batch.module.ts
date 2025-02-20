a00a9db17db4806d4387c90e7905ffccaf25ee70
DixonKH <khaydarovdilshod9@gmail.com>
2025-02-20 23:39:11 +0900

    feat: deploy monorepo project on docker container

--- apps/edusmart-batch/src/edusmart-batch.module.ts
+++ apps/edusmart-batch/src/edusmart-batch.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { EdusmartBatchController } from './edusmart-batch.controller';
 import { EdusmartBatchService } from './edusmart-batch.service';
 import { ConfigModule } from "@nestjs/config";
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot()
+    ConfigModule.forRoot(),
+    DatabaseModule
   ],
   controllers: [EdusmartBatchController],
   providers: [EdusmartBatchService],
